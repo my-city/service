f4d182f87e9074e9a69d0492eabcc9de5fe9b97d
Aram Koukia <aramkoukia@gmail.com>
2017-03-13 13:44:25 -0700

    added some documentDb utility classes

--- MyCity.Service/routes/trail.ts
+++ MyCity.Service/routes/trail.ts
@@ -4,6 +4,12 @@
 import express = require('express');
 const router = express.Router();
 
+var DocumentDBClient = require('documentdb').DocumentClient;
+var async = require('async');
+
+
+
+
 router.get('/', (req: express.Request, res: express.Response) => {
     res.send("List of trails");
 });
